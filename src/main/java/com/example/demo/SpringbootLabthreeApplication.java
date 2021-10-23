b034a8ab033169067161e2db750edb476f32c8e6
cspruil1 <cspruil1@Srv19StuGen-004.cpcc.edu>
2021-10-23 18:22:56 -0400

    initial release

---
+++ src/main/java/com/example/demo/SpringbootLabthreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLabthreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLabthreeApplication.class, args);
+	}
+
+}
